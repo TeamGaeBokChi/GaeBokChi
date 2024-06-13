2d3014d08b3ae881d63b88f8f4ab617b24bd2c40
LJY <leejyeyoung@naver.com>
2024-06-13 16:15:32 +0900

    6/13

---
+++ spring2/src/test/java/com/itwill/spring2/repository/CommentDaoTest.java
@@ -0,0 +1,56 @@
+package com.itwill.spring2.repository;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/application-context.xml" })
+public class CommentDaoTest {
+
+	@Autowired // 스프링 컨테이너가 생성/관리하는 빈을 주입받음.
+	private CommentDao commentDao;
+
+//    @Test
+	public void testSelect() {
+		Assertions.assertNotNull(commentDao);
+
+		List<Comment> list = commentDao.selectByPostId(102);
+		for (Comment c : list) {
+			log.debug(c.toString());
+		}
+	}
+
+    @Test
+	public void testInsert() {
+		Comment comment = Comment.builder().postId(4).username("admin").ctext("댓글 입력 ").build();
+		int result = commentDao.insert(comment);
+		Assertions.assertEquals(1, result);
+	}
+
+//    @Test
+	public void testUpdate() {
+		Comment comment = Comment.builder().id(3).ctext("update test ... ㅎㅎㅎ").build();
+		int result = commentDao.update(comment);
+		Assertions.assertEquals(1, result);
+	}
+
+//	@Test
+	public void testDeleteById() {
+		int result = commentDao.deleteById(2);
+		Assertions.assertEquals(1, result);
+	}
+
+}
+
+
+
+
