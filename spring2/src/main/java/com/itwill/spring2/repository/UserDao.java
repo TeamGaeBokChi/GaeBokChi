4b6b32398345374bdfa83ea3e91caaa1931a9815
BaenJin <125777492+BaenJin@users.noreply.github.com>
2024-07-05 11:24:53 +0900

    Delete spring2 directory

--- spring2/src/main/java/com/itwill/spring2/repository/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.itwill.spring2.repository;
-
-public interface UserDao {
-    
-    User selectByUserid(String userid);
-    int insert(User user);
-    User selectByUseridAndPassword(User user);
-
-}
